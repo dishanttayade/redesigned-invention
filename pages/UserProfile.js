9ee6dd6f72c7fc633cb79e510c5ae0a5a89212bf
dshanttayade <dishanttayade1@outlook.com>
2022-12-04 06:02:30 +0530

    kind of final

--- pages/UserProfile.js
+++ pages/UserProfile.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 const UserProfile = () => {
   const builder = false;
   const projects = [
@@ -8,8 +9,14 @@ const UserProfile = () => {
     "zxfghjkp",
   ];
 
-  const ownProjects = [['123we', 'SomeName', 'The location over there', '']]
+  const ownProjects = [{'id' : '123we', 'projectname' :'SomeName','location' :'The location over there', 'description': 'ygui9uygtfccguhiopijuyfdghjk', 'prospectus' :'https://rapidapi.com/search/real-estate' }]
   const isBuilder = true;
+
+  const yetToClaimVal = 10000
+  
+  const updateInfo = () => {
+
+  }
 
   return (
     <section className="h-full gradient-form bg-orange-300 md:h-screen">
@@ -18,14 +25,28 @@ const UserProfile = () => {
           <div className="xl:w-10/12">
             <div className="block bg-white shadow-lg rounded-lg">
               <div className="md:p-12 md:mx-6">
+                <div>
+                    <p className='text-xl text-bold'>Yet to Claim</p>
+                </div>
+                <form method='POST' action=''>
+                    <div className='p-4 flex justify-around'>
+                    <input type='Number' placeholder={yetToClaimVal} className="border border-black w-50 text-2xl"  />
+                    <button
+                        onClick={updateInfo}
+                        className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700"
+                    >
+                    Claim It Now
+                    </button>
+                    </div>
+                </form>
                 <div className="text-center">
                   <table className="table-auto">
                     <thead>
                     <tr>
-                        <th>Project ID</th>
-                        <th>Project Name</th>
-                        <th>Location</th>
-                        <th>Prospectus</th>
+                        <th className="p-3">Project ID</th>
+                        <th className="p-3">Project Name</th>
+                        <th className="p-3">Location</th>
+                        <th className="p-3">Prospectus</th>
                     </tr>
                     </thead>
                     {
@@ -33,12 +54,12 @@ const UserProfile = () => {
                             return (
                                 <tbody>
                                 <tr>
-                                    <td>{project.id}</td>
-                                    <td>{project.projectname}</td>
-                                    <td>{project.location}</td>
-                                    <td>
-                                        <img src='./assets/link.svg' alt={project.prospectus} >
-
+                                    <td className="p-3">{project.id}</td>
+                                    <td className="p-3">{project.projectname}</td>
+                                    <td className="p-3">{project.location}</td>
+                                    <td className="p-3">
+                                        <img src='link.svg' alt={project.prospectus} >
+                                        {/* <a href={project.prospectus} /> */}
                                         </img>
                                     </td>
                                 </tr>
@@ -47,9 +68,6 @@ const UserProfile = () => {
                         })
                     }
                   </table>
-                <div>
-                  <p>Yet to Claim</p>
-                </div>
               </div>
             </div>
           </div>
